a2de08500f7792d4c3dfd29985e5bbb6d857e4ea
Bhavya-is-great <bdisop1234@gmail.com>
2026-03-20 03:11:12 +0530

    Auth made

--- utils/api.middleware.js
+++ utils/api.middleware.js
@@ -1,21 +1,18 @@
 import connectDB from "@/utils/db";
-import { createErrorResponse, createSuccessResponse } from "@/utils/api.util";
+import { createSuccessResponse } from "@/utils/api.util";
+import wrapAsync from "@/utils/wrapAsync.util";
 
 const apiHandler = (handler, options = {}) => {
   const { connectDb = true } = options;
 
-  return async (req, context) => {
-    try {
-      if (connectDb) {
-        await connectDB();
-      }
-
-      const result = await handler(req, context);
-      return result instanceof Response ? result : createSuccessResponse(result);
-    } catch (error) {
-      return createErrorResponse(error);
+  return wrapAsync(async (req, context) => {
+    if (connectDb) {
+      await connectDB();
     }
-  };
+
+    const result = await handler(req, context);
+    return result instanceof Response ? result : createSuccessResponse(result);
+  });
 };
 
 export default apiHandler;
